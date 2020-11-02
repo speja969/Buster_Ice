60467335c7d1f614f2022226435513fb07c89560
speja969 <62497469+speja969@users.noreply.github.com>
2020-11-02 08:23:10 +0100

    Update Bullseye_Pejax.sh

--- scripts/Bullseye_Pejax.sh
+++ scripts/Bullseye_Pejax.sh
@@ -19,39 +19,35 @@ sudo apt-get install -y software-properties-common && sudo apt-add-repository co
 
 sudo apt-get update
 
-sudo apt-get install -y openbox obconf rxvt-unicode micro mousepad compton compton-conf firefox-esr tint2 menu pulseaudio xsel numlockx pavucontrol mlocate lxappearance vlc arandr apt-file synaptic firmware-linux firmware-misc-nonfree firmware-linux-nonfree doublecmd-common xutils mesa-utils xarchiver htop gparted sysstat acpi hardinfo hddtemp gnome-disk-utility python3-pip ttf-mscorefonts-installer fonts-ubuntu fonts-ubuntu-console suckless-tools simplescreenrecorder font-manager ranger geany gdebi fbxkb mpv curl gmrun xscreensaver galternatives pnmixer sxiv scrot xsettingsd ffmpeg git wmctrl network-manager network-manager-gnome xinit xorg xserver-xorg
+sudo apt-get install -y openbox obconf rxvt-unicode micro mousepad compton compton-conf firefox-esr tint2 menu pulseaudio xsel numlockx pavucontrol mlocate lxappearance vlc arandr apt-file synaptic firmware-linux firmware-misc-nonfree firmware-linux-nonfree doublecmd-common xutils mesa-utils xarchiver htop gparted sysstat acpi hardinfo hddtemp gnome-disk-utility python3-pip ttf-mscorefonts-installer fonts-ubuntu fonts-ubuntu-console suckless-tools simplescreenrecorder font-manager ranger geany gdebi fbxkb mpv curl gmrun xscreensaver galternatives pnmixer sxiv scrot xsettingsd ffmpeg git wmctrl network-manager network-manager-gnome lightdm
 
 sudo apt install -y k3b --no-install-recommends 
 sudo apt install -y transmission-qt --no-install-recommends
 sudo apt install -y pcmanfm-qt --no-install-recommends
 sudo apt autoremove
 
-echo "exec openbox-session" >> ~/.xinitrc
+cp ~/Bullseye_Pejax_v2/dotfiles/tint2rc ~/.config/tint2
 
-sudo chmod 777 ~/.xinitrc
+cp ~/Bullseye_Pejax_v2/dotfiles/compton.conf ~/.config/
 
-cp ~/Bullseye_Pejax/dotfiles/tint2rc ~/.config/tint2
+cp ~/Bullseye_Pejax_v2/dotfiles/autostart.sh ~/.config/openbox
 
-cp ~/Bullseye_Pejax/dotfiles/compton.conf ~/.config/
-
-cp ~/Bullseye_Pejax/dotfiles/autostart.sh ~/.config/openbox
-
-cp ~/Bullseye_Pejax/dotfiles/.Xresources ~/
+cp ~/Bullseye_Pejax_v2/dotfiles/.Xresources ~/
 sudo chmod 777 ~/.Xresources
 
-cp ~/Bullseye_Pejax/dotfiles/rc.xml ~/.config/openbox
+cp ~/Bullseye_Pejax_v2/dotfiles/rc.xml ~/.config/openbox
 
-cp ~/Bullseye_Pejax/dotfiles/menu.xml ~/.config/openbox
+cp ~/Bullseye_Pejax_v2/dotfiles/menu.xml ~/.config/openbox
 
-cp ~/Bullseye_Pejax/dotfiles/keyboard.sh ~/
+cp ~/Bullseye_Pejax_v2/dotfiles/keyboard.sh ~/
 
-sudo cp ~/Bullseye_Pejax/dotfiles/rs.png /usr/share/fbxkb/images/rs.png
+sudo cp ~/Bullseye_Pejax_v2/dotfiles/rs.png /usr/share/fbxkb/images/rs.png
 
 mkdir ~/.config/mpv
 
-cp ~/Bullseye_Pejax/dotfiles/mpv.conf ~/.config/mpv
+cp ~/Bullseye_Pejax_v2/dotfiles/mpv.conf ~/.config/mpv
 
-sudo cp ~/Bullseye_Pejax/dotfiles/ncmpcpp_48x48.png /usr/share/icons
+sudo cp ~/Bullseye_Pejax_v2/dotfiles/ncmpcpp_48x48.png /usr/share/icons
 
 sudo chmod 777 /usr/share/icons/ncmpcpp_48x48.png
 
@@ -77,22 +73,22 @@ sudo chmod 777 ~/keyboard.sh
 mkdir -p ~/Documents/DEB
 
 ## debinfo -- prikaz resursa pri otvaranju terminala
-sudo cp ~/Bullseye_Pejax/scripts/debinfo /usr/bin
+sudo cp ~/Bullseye_Pejax_v2/scripts/debinfo /usr/bin
 sudo chmod 777 /usr/bin/debinfo
 echo debinfo >> ~/.bashrc
 
 ## instalacija comptona
-cp ~/Bullseye_Pejax/scripts/install_compton.sh ~/.scripts
+cp ~/Bullseye_Pejax_v2/scripts/install_compton.sh ~/.scripts
 mkdir ~/bin
-cp ~/Bullseye_Pejax/dotfiles/start-compton.sh ~/bin
+cp ~/Bullseye_Pejax_v2/dotfiles/start-compton.sh ~/bin
 sudo chmod --recursive 777 ~/bin
 
 ## instalacija ncmpcpp
 sudo apt install -y mpd ncmpcpp
 mkdir ~/.mpd
 mkdir ~/.ncmpcpp
-cp ~/Bullseye_Pejax/dotfiles/config ~/.ncmpcpp
-cp ~/Bullseye_Pejax/dotfiles/mpd.conf ~/.mpd
+cp ~/Bullseye_Pejax_v2/dotfiles/config ~/.ncmpcpp
+cp ~/Bullseye_Pejax_v2/dotfiles/mpd.conf ~/.mpd
 touch ~/.mpd/mpd.db ~/.mpd/mpd.log ~/.mpd/mpd.pid
 sudo chmod --recursive 777 ~/.mpd
 sudo chmod --recursive 777 ~/.ncmpcpp
@@ -102,17 +98,17 @@ sudo sed -i 's!Terminal=true!Terminal=false!' /usr/share/applications/ncmpcpp.de
 echo "Icon=/usr/share/icons/ncmpcpp_48x48.png" >> /usr/share/applications/ncmpcpp.desktop
 
 ## screeny
-cp ~/Bullseye_Pejax/scripts/screeny ~/.scripts
+cp ~/Bullseye_Pejax_v2/scripts/screeny ~/.scripts
 
 ## script for reinstall youtube-dl
-cp ~/Bullseye_Pejax/scripts/reinstall_youtube-dl.sh ~/.scripts
+cp ~/Bullseye_Pejax_v2/scripts/reinstall_youtube-dl.sh ~/.scripts
 
 ## Geany theme settings
-cp ~/Buster_Pejax_v2/scripts/settings_geany ~/.scripts
+cp ~/Bullseye_Pejax_v2/scripts/settings_geany ~/.scripts
 sudo ./settings_geany
 
 ## obmenu-generator
-cp ~/Bullseye_Pejax/scripts/obmenu-generator.sh ~/.scripts
+cp ~/Bullseye_Pejax_v2/scripts/obmenu-generator.sh ~/.scripts
 
 mkdir ~/projects
 cd ~/projects
@@ -132,7 +128,7 @@ sudo ./install.sh
 
 cd ~/projects/debian-openbox/install_wpsoffice
 sudo ./install.sh
-cp ~/Bullseye_Pejax/scripts/install_missing_wps_fonts.sh ~/.scripts
+cp ~/Bullseye_Pejax_v2/scripts/install_missing_wps_fonts.sh ~/.scripts
 cd ~/.scripts
 sudo ./install_missing_wps_fonts.sh
 
@@ -152,12 +148,12 @@ sudo chmod --recursive 777 ~/.config/nomacs
 cd ~/projects/debian-openbox/config_disable-services
 sudo ./install.sh
 
-cd ~/projects/debian-openbox/script_loginfetch
-sudo ./install.sh
+# cd ~/projects/debian-openbox/script_loginfetch
+# sudo ./install.sh
 
 # Copy wallpapers folderes
-sudo cp -r ~/Bullseye_Pejax/WALLPAPERS/Wallpapers_Debian /usr/share/backgrounds
-sudo cp -r ~/Bullseye_Pejax/WALLPAPERS/wallpapers-pixabay /usr/share/backgrounds
+sudo cp -r ~/Bullseye_Pejax_v2/WALLPAPERS/Wallpapers_Debian /usr/share/backgrounds
+sudo cp -r ~/Bullseye_Pejax_v2/WALLPAPERS/wallpapers-pixabay /usr/share/backgrounds
 
 sudo sed -i 's!wallpapers-pack1!wallpapers-pixabay!' ~/projects/debian-openbox/15_openbox_wallpaper-packs/install.sh
 sudo sed -i 's!bl-colorful-aptenodytes-forsteri-by-nixiepro.png!kuala-lumpur-1820944_1920.jpg!' ~/projects/debian-openbox/15_openbox_wallpaper-packs/install.sh
@@ -167,7 +163,7 @@ sudo sed -i 's!/usr/share/backgrounds/wallpapers-pack1/bl-colorful-aptenodytes-f
 cd ~/projects/debian-openbox/15_openbox_wallpaper-packs
 sudo ./install.sh
 
-sudo cp ~/Bullseye_Pejax/WALLPAPERS/Wallpapers_Debian/lightdm_login.jpg /usr/share/images/desktop-base
+sudo cp ~/Bullseye_Pejax_v2/WALLPAPERS/Wallpapers_Debian/lightdm_login.jpg /usr/share/images/desktop-base
 sudo chmod 777 /usr/share/images/desktop-base/lightdm_login.jpg
 sudo sed -i 's!#background=!background=/usr/share/images/desktop-base/lightdm_login.jpg!' /etc/lightdm/lightdm-gtk-greeter.conf
 
@@ -195,7 +191,8 @@ echo "Exec=su-to-root -X -c /usr/sbin/gparted" > /tmp/gparted_replacement
 sudo sed -i "s/^.*Exec=/usr/sbin/gparted.*$/$(cat /tmp/gparted_replacement)/" /usr/share/applications/gparted.desktop
 
 mkdir -p ~/.urxvt/ext
-cp -R ~/Bullseye_Pejax/ext ~/.urxvt/ext/
-sudo chmod -R 777 ~/.urxvt
-sudo chmod 777 ~/.Xresources
-cd ~; ln -s .Xresources .Xdefaults-$(hostname)
+cp -p ~/Bullseye_Pejax_v2/ext/* ~/.urxvt/ext/
+
+sudo chown -R $(logname):$(logname) /home/$(logname)/
+find /home/$(logname) -name '.*' | xargs sudo chown $(logname):$(logname)
+find /home/$(logname) -type f | xargs sudo chmod 700
